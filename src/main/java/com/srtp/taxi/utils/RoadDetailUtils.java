b0bc8925aa298a041b69341b1e0bb3f5636c3324
starn <1172636187@qq.com>
2020-10-15 02:20:00 +0800

    更新距离和时间

--- src/main/java/com/srtp/taxi/utils/RoadDetailUtils.java
+++ src/main/java/com/srtp/taxi/utils/RoadDetailUtils.java
@@ -15,14 +15,27 @@ public class RoadDetailUtils {
     }
 
     public static int getDistance(double startLng,double startLat,double endLng,double endLat){
-        JsonRootBean js=getRoadDetail("https://apis.map.qq.com/ws/distance/v1/matrix/?mode=driving&from="+startLat+","+startLng+"&to="+endLat+","+endLng+"&key=MMBBZ-BIALF-NTNJV-JJ74D-APHP2-UHFSM"
-        ,HttpMethod.GET,new LinkedMultiValueMap<>());
+        JsonRootBean js;
+        while (true){
+            js=getRoadDetail("https://apis.map.qq.com/ws/distance/v1/matrix/?mode=driving&from="+startLat+","+startLng+"&to="+endLat+","+endLng+"&key=MMBBZ-BIALF-NTNJV-JJ74D-APHP2-UHFSM"
+                    ,HttpMethod.GET,new LinkedMultiValueMap<>());
+            if(js!=null){
+                break;
+            }
+        }
+
         return js.getResult().getRows().get(0).getElements().get(0).getDistance();
     }
 
     public static int getTime(double startLng,double startLat,double endLng,double endLat){
-        JsonRootBean js=getRoadDetail("https://apis.map.qq.com/ws/distance/v1/matrix/?mode=driving&from="+startLat+","+startLng+"&to="+endLat+","+endLng+"&key=MMBBZ-BIALF-NTNJV-JJ74D-APHP2-UHFSM"
+        JsonRootBean js;
+        while (true){
+        js=getRoadDetail("https://apis.map.qq.com/ws/distance/v1/matrix/?mode=driving&from="+startLat+","+startLng+"&to="+endLat+","+endLng+"&key=MMBBZ-BIALF-NTNJV-JJ74D-APHP2-UHFSM"
                 ,HttpMethod.GET,new LinkedMultiValueMap<>());
+            if(js!=null){
+                break;
+            }
+        }
         return js.getResult().getRows().get(0).getElements().get(0).getDuration();
     }
 
